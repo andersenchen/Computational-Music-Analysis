85d80fb8444da5e23152e388d4ab9b9871df0142
Sam Boosalis <samboosalis@gmail.com>
2012-09-21 01:50:43 -0400

    more scripty

--- freqs.py
+++ freqs.py
@@ -30,31 +30,35 @@ max(audio) == 2**31
 # half(fftfreq(n,d)) => [0/n 1/n 2/n ... 1/2) / d
 # half(fftfreq(8,1)) => [0/8 1/8 2/8 3/8]
 
-def main(filename, gui):
-    window_size = 1024
+if __name__!='__main__': exit(1)
 
-    from scipy.io import wavfile
-    sample_rate, audio = wavfile.read(filename)
+
+import argparse
+parser = argparse.ArgumentParser(description="fft music")
+parser.add_argument('-gui', dest='gui', action='store_true', default=False, help='whether to show plot')
+parser.add_argument('filename', help='the audio file to fft')
+args = parser.parse_args()
+
+filename = args.filename
+gui = args.gui
+
+
+window_size = 1024
+
+from scipy.io import wavfile
+sample_rate, audio = wavfile.read(filename)
     #audio = audio - mean(audio) #normalize
 
     #eg 44100 // 1024 * 1024 == 4032 (ignore last few samples for consistent signal size)
-    for t in arange(0, (sample_rate // window_size) * window_size, window_size):
-        t = t + 48000
-        signal = audio[t : t+window_size]
-        freqs, spectrum = fft_(signal, window_size, sample_rate)
+for t in arange(0, (sample_rate // window_size) * window_size, window_size):
+    t = t + 48000
+    signal = audio[t : t+window_size]
+    freqs, spectrum = fft_(signal, window_size, sample_rate)
         #print filter(lambda x: smooth(x[1], eps=1), zip(half(freqs), half(spectrum)) )
-
-        top_freqs = sorted( zip(map(note, half(freqs)), half(spectrum)), reverse=True, key=lambda ue: ue[1] )
-        print_freqs(top_freqs)
-        if gui: plot_fft(signal, window_size, sample_rate)
-
-        break
-
-if __name__=='__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description="fft music")
-    parser.add_argument('-gui', dest='gui', action='store_true', default=False, help='whether to show plot')
-    parser.add_argument('filename', help='the audio file to fft')
-    args = parser.parse_args()
     
-    main(args.filename, args.gui)
+    top_freqs = sorted( zip(map(note, half(freqs)), half(spectrum)), reverse=True, key=lambda ue: ue[1] )
+    print_freqs(top_freqs)
+    if gui: plot_fft(signal, window_size, sample_rate)
+    
+    break
+
