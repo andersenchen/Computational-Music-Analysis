e67f78abdb06b04ee0291bc677636f96b42ea42c
Sam Boosalis <samboosalis@gmail.com>
2012-09-21 00:55:13 -0400

    argparse. gitignore

--- music.py
+++ music.py
@@ -17,10 +17,11 @@ def piano():
 
 #:: [(key, freq)]
 notes = piano()
+freqs = dict(notes)
 
-key, freq = notes[49] 
+key, frequency = notes[49] 
 assert 'a4' == key
-assert about( 440, freq )
+assert about( 440, frequency )
 
 
 # note 440 = a4
@@ -31,9 +32,14 @@ def note(freq):
     
     return reduce((lambda x,y: x if nearer(freq, x[F],y[F]) else y), notes)
 
-print note(440)
-key, freq = note(440)
+key, frequency = note(440)
 assert 'a4' == key
+
+
+def freq(note): return freqs[note] if note in freqs else None
+
+assert about( freq('a4'), 440 )
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # sine wave <= sox
@@ -61,25 +67,34 @@ max(audio) == 2**31
 # half(fftfreq(n,d)) => [0/n 1/n 2/n ... 1/2) / d
 # half(fftfreq(8,1)) => [0/8 1/8 2/8 3/8]
 
-def main(filename):
+def print_freqs(freqs, k=10, simplify=round):
+    for ((n,u),e) in freqs[:k]: print '\t'.join([n, str(simplify(e))])
+
+def main(filename, gui):
     window_size = 1024
 
     from scipy.io import wavfile
     sample_rate, audio = wavfile.read(filename)
-    audio = audio - mean(audio) #normalize
+    #audio = audio - mean(audio) #normalize
 
     #eg 44100 // 1024 * 1024 == 4032 (ignore last few samples for consistent signal size)
     for t in arange(0, (sample_rate // window_size) * window_size, window_size):
-
+        t = t + 48000
         signal = audio[t : t+window_size]
         freqs, spectrum = fft_(signal, window_size, sample_rate)
         #print filter(lambda x: smooth(x[1], eps=1), zip(half(freqs), half(spectrum)) )
-        print sorted( zip(half(freqs), half(spectrum)), reverse=True, key=lambda ue: ue[0] )[0:3]
-        plot_fft(signal, window_size, sample_rate)
+
+        top_freqs = sorted( zip(map(note, half(freqs)), half(spectrum)), reverse=True, key=lambda ue: ue[1] )
+        print_freqs(top_freqs)
+        if gui: plot_fft(signal, window_size, sample_rate)
+
         break
 
 if __name__=='__main__':
-    import sys
-    filename = sys.argv[1] if len(sys.argv)>1 else 'short.wav'
+    import argparse
+    parser = argparse.ArgumentParser(description="fft music")
+    parser.add_argument('-gui', dest='gui', action='store_true', default=False, help='whether to show plot')
+    parser.add_argument('filename', help='the audio file to fft')
+    args = parser.parse_args()
     
-    main(filename)
+    main(args.filename, args.gui)
