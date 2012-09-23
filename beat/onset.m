c0b13e6c086ebefb585d405c4003998bc23725d4
John Thickstun <johnthickstun@John-Thickstuns-MacBook-Pro.local>
2012-09-23 11:58:52 -0400

    More cleanup. More doc. More graphs.

--- beat/onset.m
+++ beat/onset.m
@@ -5,12 +5,16 @@ clear all;
 [y, Fs] = wavread('scale.wav');
 
 % only get first channel (two if stereo)
-channel = y(:, 1);
+% only get first 15 secs (current onset computation is slow)
+channel = y(1:(min(end,Fs*15)),1);
+
+%% Time plot
+plot(channel)
 
 %% STFT
 
-% size of each window
-windowSize = 1024;
+% size of each window (can play with this if you want)
+windowSize = 2^10;
 
 nWindows = floor(numel(channel)/windowSize*2);
 spec = zeros(windowSize,nWindows-2);
@@ -34,17 +38,19 @@ processed = [];
 lockout = -1;
 for i = 1:size(spec,2)
     i
-    onset = 0;
+    onset = 0; % guess no onset
     for bucket = 1:windowSize
         history = spec(bucket,floor(max(1,i-Fs/windowSize)):i);
         if(spec(bucket,i) > mean(history) + 6*std(history))
-            onset = 1;
+            onset = 1; % detected energy variation > 6sigma from mean
         end
     end
     if (onset == 1 && lockout < 0)
         fprintf('onset\n')
         processed = [processed i*windowSize/2];
-        lockout = 10;
+        
+        % don't let onsets pile up on each other
+        lockout = floor(Fs/(windowSize*4))
     end
     
     lockout = lockout - 1;
@@ -53,19 +59,14 @@ end
 %% Mark onsets aurally
 
 output = channel;
-lengthS = 1000/44100;
-t1 = 0:1:44100*lengthS;
-y1 = 3/4*sin((2*pi/(44100/440))*t1);
-y2 = y1*4;
-st1 = size(t1,2);
+mark = 3/4*sin((2*pi/(44100/440))*(0:1:floor(Fs/44.1)));
 for i = 1:size(processed,2)
-    for j = 1:st1
+    for j = 1:numel(mark)
         ind = processed(i)+j-250;
         output(ind) = output(ind)*0.25;
-        output(ind) = output(ind)+y1(j);
+        output(ind) = output(ind)+mark(j);
     end
 end
 
 %% Write annotated .wav file
 wavwrite(output, Fs, 'onsets.wav');
-
