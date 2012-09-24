e3de3f6cf55785e3b263e4b9f2c385059a580e6e
Sam Boosalis <samboosalis@gmail.com>
2012-09-24 14:44:04 -0400

    simplify, clean up, make pythonic. nice command line interface

--- project_management/emailer.py
+++ project_management/emailer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import smtplib
 import string
 import time
@@ -5,49 +6,43 @@ import random
 import math
 import sys
 import re
+import argparse
 
-SUBJECT = "PLEASE CONTACT:"
-FROM = "shakespeare@shakespeare.info"
-
-phoneNumbers = [];
-input = open('numbers.txt', 'r')
-p = re.compile('[0-9]');
-myNumbers = "";
-for line in input:
-    if len(line.strip()) > 0:
-        phoneNumbers.append((myNumbers.join(p.findall(line))))
-    ##print line;
-file = open('test.txt', 'r')
-suffixes = ["@messaging.sprintpcs.com", "@vtext.com", "@txt.att.net", "@tmomail.net", "@vmobl.com", "@email.uscc.net", "@messaging.nextel.com", "@myboostmobile.com", "@message.alltel.com"]
-
+SUBJECT = ''
+spammers = ["messaging.sprintpcs.com", "vtext.com", "txt.att.net", "tmomail.net", "vmobl.com", "email.uscc.net", "messaging.nextel.com", "myboostmobile.com", "message.alltel.com"]
 server = smtplib.SMTP("mail-relay.brown.edu")
 startTime = time.time()
-endHour = 60*60*10;
+endHour = 60*60*10
 
-for line in file.split("."):
-    if len(line.strip()) < 1:
-        continue;
-    FROM = "god" + str(random.randint(0,10000))+"@heaven.info"
+ap = argparse.ArgumentParser(description="project management software to spam employee with texts")
+ap.add_argument('message', nargs='+', help='what to spam')
+ap.add_argument('-to', dest='phonenumbers', nargs='+', help='who to spam')
+args = ap.parse_args()
+message = ' '.join(args.message)
+phonenumbers = args.phonenumbers
+print args
+
+for phonenumber in phonenumbers:
+    FROM = "god" + str(random.randint(0,10000))+"heaven.info"
     if time.time()-startTime > endHour:
-        print "Program has run!"
+        server.quit()
         sys.exit(0)
-    print line
-    for entry in suffixes:
-        for phoneNumber in phoneNumbers:
-            text = line
-            TO = str(phoneNumber)+str(entry);
-            BODY = string.join((
-                "From: %s" % FROM,
-                "To: %s" % TO,
+    print phonenumber
+    
+    for spammer in spammers:
+        TO = phonenumber + '@' + spammer
+        BODY = string.join((
+                "From: %s"    % FROM,
+                "To: %s"      % TO,
                 "Subject: %s" % SUBJECT ,
                 "",
-                text
+                message
                 ), "\r\n")
-            
-            print TO
-            server.sendmail(FROM, TO, BODY)
+        
+        print TO
+        server.sendmail(FROM, TO, BODY)
+
     t = math.fabs(random.gauss(15,5))
     print t
     time.sleep(t)
-server.quit()
 
