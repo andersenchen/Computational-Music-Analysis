a36c79de9ce92cef039d098a4367e5807eb64a19
Sam Boosalis <samboosalis@gmail.com>
2012-11-02 14:34:18 -0400

    black key dataset. renaming.

--- pitch/detect.py
+++ pitch/detect.py
@@ -26,14 +26,21 @@ OUT_DIR = 'out'
 IMAGE_DIR = 'images'
 save = False
 
+def stats(A):
+    print
+    print 'min  = %.3f' % A.min()
+    print 'mean = %.3f' % A.mean()
+    print 'std  = %.3f' % A.std()
+    print 'max  = %.3f' % A.max()
+
 pitch_detection_algorithms = ['nmf','pinv', 'gd'] # enum
 def is_pitch_detection_algorithm(s):
     if s in pitch_detection_algorithms: return s
     else: raise ValueError()
 
-datasets = ['all', 'octave', 'piano','cello'] # enum
+dataset_dirs = ['all', 'white', 'black', 'piano','cello'] # enum
 def is_dataset(s):
-    if s in datasets: return s
+    if s in dataset_dirs: return s
     else: raise ValueError()    
 
 if __name__=='__main__':
@@ -43,9 +50,8 @@ if __name__=='__main__':
     p.add_argument('-how', type=is_pitch_detection_algorithm,
                    default='nmf',
                    help='nmf | pinv | gd')
-    p.add_argument('-data', type=is_dataset,
-                   default='octave',
-                   help='octave | piano | cello | all')
+    p.add_argument('-data', type=is_dataset, #nargs='*', #rets list
+                   help='piano | white | black | cello | all')
     p.add_argument('-ioff', action='store_true', dest='ioff', help='ioff = interact with plot (ion = interact with program)')
 
     args = p.parse_args()
@@ -55,27 +61,31 @@ if __name__=='__main__':
 
     # train classifier
     piano  = [glob('train/piano/*.wav')]
-    octave = [glob('train/octave/*.wav')]
+    white  = [glob('train/white/*.wav')]
+    black  = [glob('train/black/*.wav')]
     cello  = [glob('train/cello/*.wav')]
-    everything = octave + cello
+    everything = cello + white
     dataset = {'all' : everything,
                'piano' : piano,
-               'octave' : octave,
+               'white' : white,
+               'black' : black,
                'cello' : cello,
                }[data]
-else:
-    dataset = [glob('train/octave/*.wav')]
-    file = 'chord.wav'
-    how = 'nmf'
 
-# classifier :: |notes| by window_size
+else:
+    #dataset = [glob('train/octave/*.wav')]
+    #file = 'chord.wav'
+    #how = 'nmf'
+    pass
+
+# classifier : |notes| by window_size
 classifier, freqs = train_joint(dataset)
 
 # read file
 spectrum, sample_rate = process_wav(file)
 
 # consts
-nWindows = spectrum.shape[0]
+nWindows, _ = spectrum.shape
 
 # pseudoinverse solution
 # solve Ax=b for x given A,b
@@ -103,74 +113,11 @@ def pitch_pinv(classifier, spectrum):
     return t(X), Ai
 
 
-# gradient descent solution (ie with additive update)
-def pitch_gd(classifier, spectrum, iters=100, stepsize=100, eps=1e-12, delta=1e-6, alpha=1e-2):
-    d, sr = classifier.shape
-    A = t(classifier.copy())
-    X = (1/d) * ones((d, nWindows))
-    B = t(spectrum.copy() / sum(spectrum))
-    
-    X, B = X[:,:-1], B[:,:-1]
-    stickiness = zeros(X.shape)
-
-    print
-    print 'GD...'
-    for _ in xrange(iters):
-        
-        # d/dx    ||Ax - b||^2 + sum log x
-        #  =  2A^T||Ax - b||   + sum 1/x
-        likelihood = mul( 2 * t(A), mul(A,X) - B )
-        
-        # wolfram alpha  d/dx -log( (2*e^(-(x-y)^2)) / (1+e^(-(x-y)^2)) )
-        #  where x = x[i][t] . y = x[i][t-1]
-        # curr_minus_prev[t][i] = X[i][t] - X[i][t-1] 
-        curr_minus_prev = X[:,:-1] - X[:,1:]
-        stickiness_numer = 2 * curr_minus_prev * exp(curr_minus_prev**2)
-        stickiness_denom = 1 + exp(curr_minus_prev**2)
-        # P( x[0] ) = uniform
-        stickiness[:,0 ] = 1/d
-        # P( x[t] | x[t-1] ) = S( ||x[t] - x[t-1]||^2 )
-        stickiness[:,1:] = stickiness_numer / stickiness_denom
-        
-        # additive update
-        update     = likelihood + alpha * stickiness
-        _X = X - stepsize*update
-        
-        # project onto feasible subspace
-        # project onto nonnegative subspace
-        #  X >= 0
-        #  |nonnegative R^d| / |R^d| = (1/2)^d  ->  nonnegative space is sparse!
-        _X[_X < delta] = delta
-        # project onto unit simplex = dirichlet support
-        #  sum X over notes (not time) == 1
-        #normalizer = sum(X, axis=ROW) # collapse rows i.e. (m,n) => (1,n)
-        #_X = _X / normalizer
-        
-        # dynamic stepsize
-        stepsize = stepsize * 0.9
-
-        # convergence
-        diff = abs(sum(_X - X))
-        #if diff < eps: return X,A
-
-        X = _X
-        
-    print 'diff:',diff
-    #print normalizer[:]
-    params = {'iters':iters,
-              'stepsize':stepsize,
-              'alpha':alpha,
-              'eps':eps,
-              'delta':delta
-              }
-              
-    return X, A, params
-
 # nmf solution (ie with multiplicative update)
 # solve Ax=b for x
 #  where x : nonnegative
 def pitch_nmf(classifier, spectrum, iters=50):
-    d, sr = classifier.shape
+    d, window_size = classifier.shape
     A = t(classifier)
     X = (1/d) * ones((d, nWindows))
     B = t(spectrum / sum(spectrum))
@@ -181,13 +128,13 @@ def pitch_nmf(classifier, spectrum, iters=50):
 
     print
     print 'NMF...'
-    print '|notes|', d          #eg 8
-    print 'sample rate', sr     #eg 4096
-    print '|windows|', nWindows #eg 119
+    print '|notes|', d           #eg 8
+    print 'sam/win', window_size #eg 4096
+    print '|windows|', nWindows  #eg 119
     print 'A', A.shape #eg 4096, 8
     print 'X', X.shape #eg 8, 119
     print 'B', B.shape #eg 4096, 119
-    
+     
     # jointly solve Ax=b forall samples
     # multiplicative update with euclidean distance
     for _ in xrange(iters): # until convergence
@@ -205,6 +152,84 @@ def pitch_nmf(classifier, spectrum, iters=50):
     params = {'iters':iters,
               }
 
+    return X, A, params
+
+
+# gradient descent solution (ie with additive update)
+def pitch_gd(classifier, spectrum, iters=50, stepsize=100, eps=1e-12, delta=1e-6, alpha=0):
+    n, _ = spectrum.shape
+    d, sr = classifier.shape
+    A = t(classifier.copy())
+    X = (1/d) * ones((d, n))
+    B = t(spectrum.copy() / sum(spectrum))
+    
+    X, B = X[:,:-1], B[:,:-1]
+    prior = zeros(X.shape)
+    
+    print
+    print 'GD...'
+    for _ in xrange(iters):
+        
+        # gaussian distance likelihood
+        # P( b | A,x ) = N[Ax,1]( b )
+        # d/dx    ||Ax - b||^2 + sum log x
+        #  =  2A^T||Ax - b||   + sum 1/x
+        likelihood = mul( 2 * t(A), mul(A,X) - B )
+
+        # markov stickiness prior
+        # P( x[t] | x[t-1] ) = 
+        #  scaled translated complemented logistic( ||x[t] - x[t-1]||^2 )
+        # wolfram alpha
+        #  d/dx -log( (2*e^(-(x-y)^2)) / (1+e^(-(x-y)^2)) )
+        time_diff = X[:,1:] - X[:,:-1]
+        stats(time_diff)
+        #diff = zeros((d,nWindows+1))
+        for (i,j) in ndindex((d, n-3)):
+            s = j+1
+            #  t-1 in 0..n-3
+            # (j   in 0..n-3)
+            #  t   in 1..n-2
+            #  t+1 in 2..n-1
+            ab = X[i,s]   - X[i,s-1]
+            bc = X[i,s+1] - X[i,s]
+            numer_ab = 2 * ab * exp(ab**2)
+            denom_ab = 1 + exp(ab**2)
+            numer_bc = 2 * bc * exp(bc**2)
+            denom_bc = 1 + exp(bc**2)
+            prior[i,s] = - (numer_ab / denom_ab) - (numer_bc / denom_bc)
+            
+        # additive update
+        update = likelihood + alpha * prior
+        _X = X - stepsize*update
+        
+        # project onto feasible subspace
+        # project onto nonnegative subspace
+        #  X >= 0
+        #  |nonnegative R^d| / |R^d| = (1/2)^d  ->  nonnegative space is sparse!
+        _X[_X < delta] = delta
+        # project onto unit simplex = dirichlet support
+        #  sum X over notes (not time) == 1
+        normalizer = sum(X) # collapse rows i.e. (m,n) => (1,n)
+        _X = _X / normalizer
+        
+        # dynamic stepsize
+        stepsize = stepsize * 0.9
+
+        # convergence
+        iter_diff = abs(sum(_X - X))
+        #if iter_diff < eps: return X,A
+
+        X = _X
+        
+    print 'diff:',iter_diff
+    #print normalizer[:]
+    params = {'iters':iters,
+              'stepsize':stepsize,
+              'alpha':alpha,
+              'eps':eps,
+              'delta':delta
+              }
+              
     return X, A, params
 
 
@@ -249,7 +274,7 @@ def d2(x, freqs, sample_rate, window_size, title=how):
     axes.imshow(x,
                 cmap=cm.jet, origin='lower', aspect='auto', interpolation='nearest')
     
-    axes.set_title('Transcription, %s' % title)
+    axes.set_title(title)
     axes.get_xaxis().set_major_locator(
         LinearLocator(1 + ceil(n_windows/window_rate)))
     axes.get_xaxis().set_major_formatter(
@@ -295,8 +320,10 @@ if __name__=='__main__':
 
     x,a,params = pitch(classifier, spectrum, how=how)
     #x = threshold(x)
-    d2(x, freqs, sample_rate, window_size, title=how)
 
+    title = 'Transcription . %s . %s' % (how, file)
+    d2(x, freqs, sample_rate, window_size, title=title)
+    
     if save:
         image = 'joint, gd, chord'
         savefig( '%s/%s.png' % (IMAGE_DIR, image), bbox_inches=0 )
@@ -306,6 +333,7 @@ if __name__=='__main__':
 
     if not args.ioff:
         time.sleep(600)
+
 
 """
 
@@ -328,3 +356,4 @@ x,a = pitch_nmf(_A, B)
 d2(x, freqs, sample_rate, window_size, title=how)
 
 """
+
